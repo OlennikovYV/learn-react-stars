29e8e95a6646082a55dcb17c7e94417d6ba8fdc7
OlennikovYV <olennikovyv@mail.ru>
2022-07-07 16:41:51 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.querySelector('.root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <div className="Empty" />
+  </React.StrictMode>
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: './public/js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new copyWebpackPlugin({
+      patterns: [{ from: 'public', to: 'public' }],
+    }),
+  ],
+  devtool: 'source-map',
+};
