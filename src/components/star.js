01d60b6f4f8e922745203cc5da2380c9fe89db47
OlennikovYV <olennikovyv@mail.ru>
2022-07-14 16:04:39 +0500

    The color attribute of the <Star /> component is placed in the css file.

--- src/components/star.js
+++ src/components/star.js
@@ -2,5 +2,13 @@ import React from 'react';
 import { FaStar } from 'react-icons/fa';
 
 export function Star({ selected = false, onSelect = f => f }) {
-  return <FaStar color={selected ? 'red' : 'gray'} onClick={onSelect} />;
+  return (
+    <FaStar
+      className={
+        `star-rating__count-stars ` +
+        `star-rating__count-stars_${selected ? 'red' : 'gray'}`
+      }
+      onClick={onSelect}
+    />
+  );
 }
