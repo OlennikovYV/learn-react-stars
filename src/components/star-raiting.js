73133d3c4378c5956055103ba7ad40344e9c17e5
OlennikovYV <olennikovyv@mail.ru>
2022-07-14 15:09:44 +0500

    Visual design.

--- src/components/star-raiting.js
+++ src/components/star-raiting.js
@@ -8,7 +8,7 @@ import { Star } from './star';
 export function StarRaiting({ totalStars = 5 }) {
   const [selectedStars, setSelectedStars] = useState(0);
   return (
-    <>
+    <div className="star-rating">
       {[...Array(totalStars)].map((el, index) => (
         <Star
           key={index}
@@ -16,9 +16,9 @@ export function StarRaiting({ totalStars = 5 }) {
           onSelect={() => setSelectedStars(index + 1)}
         />
       ))}
-      <p>
+      <p className="star-rating__count-stars">
         {selectedStars} of {totalStars} stars
       </p>
-    </>
+    </div>
   );
 }
